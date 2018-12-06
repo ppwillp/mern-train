7eea8f6f28001acef1e927741779a7f5a6c12da1
Pittman <wpittman@paypal.com>
2018-12-06 14:05:53 -0600

    first commit

---
+++ routes/api/profiles.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// @route     GET api/profiles/test
+// @desc      Tests profiles routes
+// @access    Public
+router.get('/test', (req, res) => {
+  res.json({
+    msg: "profiles works"
+  });
+});
+
+module.exports = router;
